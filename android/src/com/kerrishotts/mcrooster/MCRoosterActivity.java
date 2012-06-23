13dd1c7f1f81a23ad2da4ed425a12f0c7b536a5b
Kerri Shotts <kerrishotts@gmail.com>
2012-06-23 17:01:31 -0500

    Android version

---
+++ android/src/com/kerrishotts/mcrooster/MCRoosterActivity.java
@@ -0,0 +1,14 @@
+package com.kerrishotts.mcrooster;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MCRoosterActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index-android.html");
+    }
+}
