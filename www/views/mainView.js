551d8c04afdb845b1b31c1a379919eb3ea8ad8d0
Kerri Shotts <kerrishotts@gmail.com>
2012-06-23 16:13:55 -0500

    Commenting. Attach clicks or touch ends to elements based on if the platform is known or not. Adjust sun/moon cycle.

--- www/views/mainView.js
+++ www/views/mainView.js
@@ -1,57 +1,49 @@
 /*
  * mainView's JS
+ *
+ * Author:  Kerri Shotts
+ * License: MIT
  */
- 
-// notes on Minecraft time from the wiki:
-//    Day lasts 10 minutes
-//    Dusk lasts 1.5 minutes
-//    Night lasts 7 minutes
-//    Dawn lasts 1.5 minutes
-//
-// Notes on how /we/ calculate time
-//   The total cycle takes 20 minutes from start to finish.
-//   Assuming that we don't care about Dusk/Dawn differentiation
-//   we can say that both Day and Night are 10 minutes long. 
-//
-//   Let's say, just for the fun of it, that Day occupies 6am to 6pm.
-//   Night occupies the remainder. Using a 24-hour clock, day would
-//   be 0600 to 1759, whilst night would be 1800-2359, and 0000-0559.
-//
-//   To make our internal calculations easier, we'll use a single
-//   clock variable. Time from 0 - 599 indicates daylight, and 
-//   600-1199 will indicate nighttime. Each second we can add "1"
-//   to our clock to determine the time.
-//
-//   To convert, this function will be used
-//       ((MCTIME * 72) / 3600) + 6 = hour of day; if >=24, - 24.
-//       ((MCTIME * 72) % 3600) / 60 = minute of day
-//
-//   We can't assume our interval will be called /every/ second,
-//   on-the-dot. So we must take the time it is now and subtract
-//   from it the time recorded when dawn was noticed. 
- 
+
 var APP = APP || {};
+
+// create mainView's namespace
 APP.mainView = APP.mainView || {};
 
+// mainview's properties:
 APP.mainView.time = 0;
 APP.mainView.dawnStartedAt = 0;
+
+// mainView's timer ids:
 APP.mainView.timerId = -1;
 APP.mainView.blinkCycle = 0;
 APP.mainView.animationId = -1;
 
+/**
+ *
+ * init will locate and store the various elements that belong
+ * to mainView. It will also attach any necessary events (like
+ * touchend) to buttons and the like.
+ *
+ * Finally, when finished, it sets the view's opacity to 1 so
+ * that we get a nice fade in (if the browser supports it).
+ *
+ */
 APP.mainView.init = function ()
 {
     APP.mainView.element = document.getElementById ("mainView_html");
     APP.mainView.touchToStart = document.getElementById ("mainView_touch2start");
+    APP.mainView.startButton = document.getElementById ("mainView_start");
+    APP.mainView.resetButton = document.getElementById ("mainView_reset");
     APP.mainView.clockElement = document.getElementById ("mainView_clock");
     APP.mainView.groundElement = document.getElementById ("mainView_ground");
     APP.mainView.skyElement = document.getElementById ("mainView_sky");
     APP.mainView.sunElement = document.getElementById ("mainView_sun");
     APP.mainView.moonElement = document.getElementById ("mainView_moon");
     
-    
-    APP.FW.addEventListenerTo ( "touchend", APP.mainView.touchToStart, APP.mainView.startTime );
-//    APP.FW.addEventListenerTo ( "click", APP.mainView.touchToStart, APP.mainView.startTime );
+    APP.FW.addEventListenerTo ( (APP.FW.platform=="unknown") ? "click": "touchend", APP.mainView.touchToStart, APP.mainView.startTime );
+    APP.FW.addEventListenerTo ( (APP.FW.platform=="unknown") ? "click": "touchend", APP.mainView.startButton, APP.mainView.startTime );
+    APP.FW.addEventListenerTo ( (APP.FW.platform=="unknown") ? "click": "touchend", APP.mainView.resetButton, APP.mainView.resetTime );
     
     APP.mainView.time = 0;
     
@@ -59,28 +51,83 @@ APP.mainView.init = function ()
        
 }
 
+/**
+ *
+ * Resets time. Clears any timer ids, resets time to 0, an
+ * displays the buttons that can start everything over again.
+ *
+ */
+APP.mainView.resetTime = function ()
+{
+  clearInterval ( APP.mainView.timerId );
+  clearInterval ( APP.mainView.clockTimerId );
+  clearInterval ( APP.mainView.animationId );
+  
+  APP.mainView.time = 0;
+  APP.mainView.updateClock();
+  APP.mainView.updateDisplay();
+
+  APP.mainView.startButton.style.display="block";
+  APP.mainView.touchToStart.style.display="block";
+  APP.mainView.touchToStart.style.opacity = 1;
+}
+
+/**
+ *
+ * Starts time. Records the time at which we were started in
+ * milleseconds (/1000 to get seconds). Sets time to zero.
+ * Creates our animation Ids. Finally, it hides our "start"
+ * elements to prevent tapping them without an intervening
+ * "reset" tap.
+ *
+ */
 APP.mainView.startTime = function ()
 {
   APP.mainView.dawnStartedAt = (new Date()).getTime() / 1000;
   APP.mainView.time = 0;
+  
   APP.mainView.timerId = setInterval (APP.mainView.updateTime,100);
   APP.mainView.clockTimerId = setInterval (APP.mainView.updateClock,500);
   APP.mainView.animationId = setInterval (APP.mainView.updateDisplay,1000);
+
   APP.mainView.touchToStart.style.opacity = 0;
+  APP.mainView.startButton.style.display="none";
   setTimeout ( function() { APP.mainView.touchToStart.style.display="none"; }, 255 );
 }
 
+/**
+ *
+ * Called roughly every 100ms to update the in-world time. Based on the current
+ * time (in millseconds, divided down to seconds), we can get the delta value
+ * between when we started and where we are now. Taking the remainder after
+ * dividing by 1200 returns our in-world time.
+ *
+ */
 APP.mainView.updateTime = function ()
 {
   var theCurrentTime = (new Date()).getTime() / 1000;
   var theTimeDelta = theCurrentTime - APP.mainView.dawnStartedAt;
-  var theTimeFactor = 1; //0;
+  var theTimeFactor = 1; 
   
   var theNewMinecraftTime = (theTimeDelta*theTimeFactor) % 1200;
   
   APP.mainView.time = theNewMinecraftTime;
 }
 
+/**
+ *
+ * Called at roughly every 500ms to update the clock located in the lower-right
+ * portion of the screen. Converting from in-world time to real-world time
+ * takes some fun math, which we do at the beginning of the funciton.
+ *
+ * While it is possible to display each and every minute as it occurs in-world,
+ * it turns out that (to me, at least) this is disturbing visually. We divide
+ * by 3, floor it, and then multiply again so that we only display minute
+ * changes every 3 minutes.
+ *
+ * The final portion displays the clock and handles the ":" blink cycle.
+ *
+ */
 APP.mainView.updateClock = function ()
 {
   var theCurrentMCTime = APP.mainView.time;
@@ -100,6 +147,33 @@ APP.mainView.updateClock = function ()
                                         theRealWorldMinuteString;
 }
 
+/**
+ *
+ * Called every second or so.
+ *
+ * Updates the position of the sun (or moon) as well as the color of
+ * the sky and ground, based upon the current time.
+ *
+ * The sun and moon will slowly rise and fall over a set period of
+ * time. We use Math.sin to calculate this. The sun rises and falls
+ * over a period of 800 ticks, while the moon does the same in 400
+ * ticks. This is to come close to the MineCraft day/night cycle
+ * where night itself is only 7 minutes, while daylight (including
+ * sunrise and sunset) is 13 minutes.
+ *
+ * Once the position of the sun and moon are set, we go about
+ * calculating the color of the sky and ground. These are done
+ * based on various ranges of ticks, as follows:
+ *
+ *      000 - 100:   Dawn.  Sky/Ground brighten slowly.
+ *      100 - 700:   Day.   Sky/Ground maintain bright color.
+ *      700 - 800:   Dusk.  Sky/Ground darken slowly.
+ *      800 -1159:   Night. Sky/Ground maintain dark color.
+ *
+ * Color changes are ramped over a period of 100 steps based on
+ * the original RGB triplets and their target triplets.
+ *
+ */
 APP.mainView.updateDisplay = function ()
 {
   var theSun = APP.mainView.sunElement;
@@ -111,6 +185,7 @@ APP.mainView.updateDisplay = function ()
   // calculate the position of the sun and moon according to the time of day
   if ( 0 < theTime && theTime < 800 )
   {
+    // daytime sun cycle
     theSun.style.display = "block";
     theMoon.style.display = "none";
     theSun.style.top  = 43 - (Math.sin ( (theTime/(800/180)) * Math.PI/180 ) * 35) + "%";
@@ -118,6 +193,7 @@ APP.mainView.updateDisplay = function ()
   }
   if ( 800 < theTime && theTime < 1200 )
   {
+    // nighttime moon cycle
     theSun.style.display = "none";
     theMoon.style.display = "block";
     theMoon.style.top  = 43 - (Math.sin ( ((1200-theTime)/(400/180)) * Math.PI/180 ) * 35) + "%";
@@ -126,7 +202,6 @@ APP.mainView.updateDisplay = function ()
   
 
   // calculate the colors of the sky and ground according to the time of day
-//  theTime = APP.mainView.time;
   if ( 100 < theTime && theTime< 700 )
   {
     // daytime
@@ -135,8 +210,8 @@ APP.mainView.updateDisplay = function ()
   }
   else
   {
-    // night time
-    if ( 0 < theTime && theTime < 100 )
+    // night time cycle
+    if ( 0 < theTime && theTime < 100 ) // dawn
     {
       theSky.style.backgroundColor = "rgb(" +
                                      Math.floor( (theTime) * (159/100)) + "," +
@@ -147,7 +222,7 @@ APP.mainView.updateDisplay = function ()
                                      Math.floor( (theTime) * ( 96/100) +32) + "," +
                                      Math.floor( (theTime) * ( 48/100) +16) + ")";
     }
-    if ( 700 < theTime && theTime < 800 )
+    if ( 700 < theTime && theTime < 800 ) // dusk
     {
       theSky.style.backgroundColor = "rgb(" +
                                      Math.floor( (800-theTime) * (159/100)) + "," +
@@ -157,6 +232,11 @@ APP.mainView.updateDisplay = function ()
                                      Math.floor( (800-theTime) * ( 32/100)) + "," +
                                      Math.floor( (800-theTime) * ( 96/100) +32) + "," +
                                      Math.floor( (800-theTime) * ( 48/100) +16) + ")";
+    }
+    if ( 800 < theTime && theTime < 1200 ) // dead-of-night
+    {
+      theSky.style.backgroundColor = "rgb(0,0,32)";
+      theGround.style.backgroundColor = "rgb(0,32,16)";
     }
   }
 }
