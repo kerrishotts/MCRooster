99bbec12016e651381f643d056405d670cdd19ac
Kerri Shotts <kerrishotts@gmail.com>
2012-06-23 20:46:30 -0500

    chain xmlhttprequest loads, since WP7 is so nuts about it.

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -36,6 +36,28 @@ APP.start = function ()
                 function ( data )
                 {
                   document.getElementById("rootView").innerHTML = data;
+
+					// also, load in our view's script. Note that we have to create a script
+					// element and add the text to it AND append it to the DOM to get it
+					// to execute.
+					// Also give 100ms for both views to finalize and the DOM to stabilize
+					// before calling init();
+					APP.FW.load ( "./views/mainView.js",
+								function ( data )
+								{
+									var newScript = document.createElement ("script");
+									newScript.type = "text/javascript";
+									newScript.charset = "utf-8";
+									newScript.text = data;
+									document.body.appendChild (newScript);
+									setTimeout ( APP.mainView.init,100 );
+								},
+								function ( errorCode )
+								{
+									console.log ( "Failed to load our view's script with status " + errorCode );
+								}
+								);
+
                 },
                 function ( errorCode )
                 {
@@ -43,26 +65,6 @@ APP.start = function ()
                 }
               );
               
-  // also, load in our view's script. Note that we have to create a script
-  // element and add the text to it AND append it to the DOM to get it
-  // to execute.
-  // Also give 100ms for both views to finalize and the DOM to stabilize
-  // before calling init();
-  APP.FW.load ( "./views/mainView.js",
-                function ( data )
-                {
-                  var newScript = document.createElement ("script");
-                  newScript.type = "text/javascript";
-                  newScript.charset = "utf-8";
-                  newScript.text = data;
-                  document.body.appendChild (newScript);
-                  setTimeout ( APP.mainView.init,100 );
-                },
-                function ( errorCode )
-                {
-                  console.log ( "Failed to load our view's script with status " + errorCode );
-                }
-              );
   // assuming no failures, the HTML and associated script should be loaded
   // and the animation to display it should be occurring.
 }
